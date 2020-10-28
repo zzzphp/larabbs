4af606f4a9d25ba237a8a83187090e22be840a1f
vagrant <vagrant@homestead>
2020-10-28 07:59:22 +0000

    邮箱认证成功后的提醒

--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -1,5 +1,5 @@
 @foreach(['danger', 'warning', 'success', 'info'] as $msg)
-  @if(session()->has($msg)) {
+  @if(session()->has($msg))
     <div class="flash-message">
       <p class="alert alert-{{ $msg }}">
         {{session()->get($msg)}}
