a8d4c163e9197906d5f86c56e4f694b05a25462a
vagrant <vagrant@homestead>
2020-10-30 06:01:49 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -19,9 +20,16 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $userRequest, User $user)
+    public function update(UserRequest $userRequest, User $user, ImageUploadHandler $uploadHandler)
     {
-        $user->update($userRequest->all());
+        $data = $userRequest->all();
+        if ($userRequest->avatar) {
+            $result = $uploadHandler->save($userRequest->avatar, 'avatar', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
