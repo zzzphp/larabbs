f385e5d993184363d235aba8bd3b09c469d4b5ed
vagrant <vagrant@homestead>
2020-11-02 07:20:13 +0000

    填充用户和话题数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,15 +2,29 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        // 所有用户 ID 数组
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        // 所有分类 ID 数组
+        $category_ids = \App\Models\Category::all()->pluck('id')->toArray();
+
+        $faker = app(\Faker\Generator::class);
+
+        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) use ($user_ids, $category_ids, $faker) {
+//            if ($index == 0) {
+//                // $topic->field = 'value';
+//            }
+            // 从用户ID 数组中随机取出一个并赋值
+            $topic->user_id = $faker->randomElement($user_ids);
+
+            // 话题分类，同上
+            $topic->category_id = $faker->randomElement($category_ids);
         });
 
         Topic::insert($topics->toArray());
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 获取 faker 实例 伪造者
+        $faker = app(\Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        $users = factory(User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user, $index) use ($faker, $avatars){
+                $user->avatar = $faker->randomElement($avatars);
+            });
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        $user = User::find(1);
+        $user->name = 'hongzheng';
+        $user->email = '491315686@qq.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+
+    }
+}
