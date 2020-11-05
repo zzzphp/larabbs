62a71fcacbb2a6fc10f4a573fdc86be4b8d75a0d
vagrant <vagrant@homestead>
2020-11-05 09:34:19 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -2,5 +2,10 @@
 
     function route_class()
     {
-        return str_replace('.', ',', Route::currentRouteName());
+        return str_replace('.', '-', Route::currentRouteName());
+    }
+
+    function category_nav_active($categories_id)
+    {
+        return active_class((if_route('categories.show') && if_route_param('category', $categories_id)));
     }
