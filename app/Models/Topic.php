06d1076587d4df041b69e646c373baaebf6ef9b2
vagrant <vagrant@homestead>
2020-11-18 09:53:16 +0000

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -45,9 +45,16 @@ class Topic extends Model
         return $query->orderBy('updated_at', 'desc');
     }
 
+    public function updateReplyCount()
+    {
+        $this->reply_count = $this->replies()->count();
+        $this->save();
+    }
+
     public function link($params = [])
     {
         return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
+
 
 }
