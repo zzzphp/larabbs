9f68bec3072548832d3d5801e64c1593d7d3ec93
vagrant <vagrant@homestead>
2020-12-15 05:41:31 +0000

    删除话题

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -27,5 +27,13 @@ class TopicsController extends Controller
         return new TopicResource($topic);
     }
 
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+
+        $topic->delete();
+        return response(null, 204);
+    }
+
 
 }
